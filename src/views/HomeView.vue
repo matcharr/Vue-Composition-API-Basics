e26e4c9f1c235909f5fbd6b6abc82813db989520
matcharr <mathis.charretier.42@gmail.com>
2022-04-22 13:48:43 +0200

    Options API design, Data & Methods, Composition API Data (refs) & Methods, Script Setup - Easier Way

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,81 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+
+    <div>
+      <button @click="decrementCounter" class="btn">-</button>
+      <span class="counter">{{ counter }}</span>
+      <button @click="incrementCounter" class="btn">+</button>
+    </div>
+
   </div>
 </template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  const counter = ref(0)
+
+  const incrementCounter = () => {
+    counter.value++
+  }
+
+  const decrementCounter = () => {
+    if (counter.value > 0) {
+      counter.value--
+    }
+  }
+</script>
+
+<!--<script>-->
+<!--import { ref } from 'vue'-->
+
+<!--export default {-->
+<!--  setup() {-->
+<!--    const counter = ref(0)-->
+
+<!--    const incrementCounter = () => {-->
+<!--      counter.value++-->
+<!--    }-->
+
+<!--    const decrementCounter = () => {-->
+<!--      counter.value&#45;&#45;-->
+<!--    }-->
+
+<!--    return {-->
+<!--      counter,-->
+<!--      incrementCounter,-->
+<!--      decrementCounter,-->
+<!--    }-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
+
+<!--<script>-->
+<!--export default {-->
+<!--  data() {-->
+<!--    return {-->
+<!--      counter: 0-->
+<!--    }-->
+<!--  },-->
+<!--  methods: {-->
+<!--    incrementCounter() {-->
+<!--      this.counter++-->
+<!--    },-->
+<!--    decrementCounter() {-->
+<!--      this.counter&#45;&#45;-->
+<!--    }-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+
+.btn, .counter {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
